365831f9b9dec1e92c1e05248a9aa3a8d9c187ad
Bob Brandt <bob@brandt.ie>
2016-08-04 17:00:21 +0100

    Work 2016-08-04 17:00:21

--- plugins/processes/processes.php
+++ plugins/processes/processes.php
@@ -7,18 +7,14 @@ $datas = array();
 $max = $Config->get('processes:max');
 if ($max < 1) $max = 5;
 $sort = $Config->get('processes:sort');
-if ( $sort == 'mem' ) {
-    $sort = '-pmem';
-} else {
-    $sort = '-pcpu';
-}
+if ( $sort !== 'mem' ) $sort = 'cpu';
 
 $include = $Config->get('processes:include');
 $exclude = $Config->get('processes:exclude');
-array_push($exclude,'\[.*\]');
+array_push($exclude,'\[.*\]','\\_');
 $exclude = '\('.implode('\|',$exclude).'\)';
 
-$command = 'ps -eo "pcpu,pmem,args" --noheader --sort '.$sort;
+$command = 'ps xf -o "pcpu,pmem,args" --noheader';
 if ($exclude)
     $command .= ' | grep -v "'.$exclude.'"';
 $command .= ' | sed -e "s|^\s||" -e "s|\s\+|,|g" | cut -d "," -f 1-3 | sed "s|/.*/||"';
@@ -42,11 +38,9 @@ if (!(exec($command, $ps))) {
                        'process'  => $process );  
 
         if (in_array($process, $include))
-            if (!in_array($tmp, $inc_processes))
-                array_push($inc_processes, $tmp);
+            array_push($inc_processes, $tmp);
         else
-            if (!in_array($tmp, $processes))            
-                array_push($processes, $tmp);
+            array_push($processes, $tmp);
     }
 
     $datas = array_merge($inc_processes, $processes);
