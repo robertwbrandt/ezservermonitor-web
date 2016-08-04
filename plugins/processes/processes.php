ea82cd22ce2873962a9f1e13d4f9027ecfa052a9
Bob Brandt <bob@brandt.ie>
2016-08-04 14:47:19 +0100

    Work 2016-08-04 14:47:19

--- plugins/processes/processes.php
+++ plugins/processes/processes.php
@@ -6,10 +6,11 @@ $datas = array();
 
 $max = $Config->get('processes:max');
 $sort = $Config->get('processes:sort');
-if ( $sort == 'mem' ): 
+if ( $sort == 'mem' ): {
     $sort = 'pmem';
-else:
+} else: {
     $sort = 'pcpu';
+}
 
 $include = $Config->get('processes:include');
 $exclude = $Config->get('processes:exclude');
